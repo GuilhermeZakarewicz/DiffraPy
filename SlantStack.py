f9fc2c778feb7dea62b9d0ed061fd64a6a8d082b
Guilherme Zakarewicz <guilherme.zakarewicz.97@gmail.com>
2023-03-06 21:56:42 -0300

    functions docstrings

--- SlantStack.py
+++ SlantStack.py
@@ -7,17 +7,49 @@ print('Imported SlantStack_v2')
 
 def slant(p,tau,data1,dx,dz,z_ini,x_ini):
     """
-    Calcula a semblance da reta dada definida por (tau + p*x)
-    p - tangente de theta (prof./dist.)
-    tau - profunidade (m)
-    data1 - dado [s,m]
-    dz -
-    x_ini - [coordenada do grid]
-    z_ini -
-    Saída:
-    s -
-    x_grid -
-    z_grid -
+    Computes the slant stack value for a given tau-p.
+
+    Parameters:
+    -----------
+    p : float
+        Slope of the line (angular coefficient).
+    tau : float
+        Linear coefficient of the line.
+    data1 : numpy.ndarray
+        A 2D array of shape [nz,nx] representing the reflectivity model
+        of the subsurface.
+    dx : float
+        Sampling interval in the x direction.
+    dz : float
+        Sampling interval in the z direction.
+    z_ini : int
+        Index of the first sample of the gather in the z direction.
+    x_ini : int
+        Index of the first sample of the gather in the x direction.
+
+    Returns:
+    --------
+    s : float
+        Coherence value (semblance).
+    x_grid : numpy.ndarray 
+        Indices of the samples in the x direction.
+    z_grid : numpy.ndarray 
+        Indices of the samples in the z direction.
+        
+    Notes:
+    ------
+    - This function calculates the coherence of a line parameterized by (tau,p) 
+    in relation with the reflectivity model data1. 
+    
+    - It is possible to use (x_grid,y_grid) to plot the defined line over the 
+    model and visually check the funcionality of the function.
+    
+    References:
+    -----------
+    
+    [1] Neidell, N. S. (1997). Perceptions in seismic imaging Part 2: Reflective and 
+    diffractive contributions to seismic imaging. The Leading Edge 16:8, 1121-1123
+    
     """
     
     [nz1,ntr1]=data1.shape
@@ -59,12 +91,33 @@ def slant(p,tau,data1,dx,dz,z_ini,x_ini):
 
 def slant_stack(pmin,pmax,dp,taumin,taumax,dtau,data1,dx,dt,t_ini,x_ini):
     """
-    Retorna um painel de semblane
-    e a semblance máxima para um conjunto de retas
-    pmin, pmax (angulos minimo e maximo)
-    taumin taumax (tempos -- x=0 -- minimo e maximo (em segundos))
-    data1, dx, e dt referem-se ao dado
-    x_ini é a primeira amostra em x [grid coord]
+    Computes the slant stack of a seismic dataset for a range of slownesses and
+    intercept times.
+
+    Parameters:
+    -----------
+    pmin, pmax, dp : float
+        Minimum, maximum, and step values for angular coefficient (slope of the line).
+    taumin, taumax, dtau : float
+        Minimum, maximum, and step intercept time values (linear coefficient).
+    data1 : numpy.ndarray of shape [nz,nx]
+        A 2D array of shape [nz,nx] representing the reflectivity model
+        of the subsurface.
+    dx, dt : float
+        Spatial and temporal sampling intervals.
+    t_ini, x_ini : float
+        Origin coordinates.
+    
+    Returns:
+    --------
+    S : numpy.ndarray of shape (len(tau), len(p))
+        Coherence matrix, each cell represents the coherence value for a tau-p pair.
+    pmax, taumax : float
+        Slope and intercept time values with the maximum slant stack power (highest coherence).
+    Smax : float
+        Maximum coherence value.
+    ip, itau : int
+        Indices of the maximum coherence value in the p and tau arrays.
     """
     
     p = np.arange(pmin,pmax,dp)
@@ -90,21 +143,56 @@ def slant_stack(pmin,pmax,dp,taumin,taumax,dtau,data1,dx,dt,t_ini,x_ini):
 ### NEW FUNCTIONS ####
 
 def slant_local(data1,x_ini,z_ini,dx,dz,x0,z0,p):
-    """
-    Calculates the semblance for each image point of the input data.
+        """
+    Calculates the slant stack measure at a local location specified by the coordinates x0 and z0
+    (image point), for a given slope value p.
+
+    Parameters:
+    -----------
+    data1 : numpy.ndarray of shape (nz1, ntr1)
+        A 2D array of shape [nz,nx] representing the reflectivity model
+        of the subsurface.
+    x_ini : float
+        The x coordinate of the first trace in the seismic data.
+    z_ini : float
+        The z coordinate of the first sample in the seismic data.
+    dx : float
+        The x sampling interval in the seismic data.
+    dz : float
+        The z sampling interval in the seismic data.
+    x0 : float
+        The x coordinate of the central point of the slant stack line. 
+    z0 : float
+        The z coordinate of the central point of the slant stack line.
+    p : float
+        The slowness value used to calculate the slant stack measure.
+
+    Returns:
+    --------
+    s : float
+        Coherence value (semblance).
+    x_grid : numpy.ndarray of shape (ntr1,)
+        Indices of the samples in the x direction.
+    z_grid : numpy.ndarray of shape (ntr1,)
+        Indices of the samples in the z direction.
+        
+    Notes:
+    ------
+    - This function calculates the coherence of a line of slope defined by `p` 
+    in relation with the reflectivity model data1 for a given image point (x0,z0).
     
-    Input:
-    data1 - reflectivity model 
-    x_ini - (grid coord) initial coordinate of x-axis
-    z_ini - (grid coord) initial coordinate of z-axis
-    dx,dz -
-    x0,z0 -
-    p -
+    - It differs from the `slant` function only in the way each one defines the line.
+    In this case, it is not necessary to input the intercept time, only the image point 
+    (x0,z0) and the slope (p).
     
-    Output:
-    s -
-    x_grid -
-    z_grid -
+    - It is possible to use (x_grid,y_grid) to plot the defined line over the 
+    model and visually check the funcionality of the function.
+    
+    References:
+    -----------
+    [1] Neidell, N. S. (1997). Perceptions in seismic imaging Part 2: Reflective and 
+    diffractive contributions to seismic imaging. The Leading Edge 16:8, 1121-1123
+    
     """
     
     [nz1,ntr1]=data1.shape
@@ -150,23 +238,40 @@ def slant_local(data1,x_ini,z_ini,dx,dz,x0,z0,p):
 
 def slant_local_p(data1,x_ini,z_ini,dx,dz,x0,z0,pmin,pmax,dp):
     """
-    Calcula a semblance para o ponto imagem para diferentes inclinações (pmin,pmax,dp)
+    Calculates the maximum coherence value and its corresponding slope for a given
+    data and a range of slopes. Uses the `slant_local` function to calculate the 
+    coherence values over a range of slopes.
+
+    Parameters:
+    -----------        
+    data1 : numpy.ndarray of shape (nz1, ntr1)
+        A 2D array of shape [nz,nx] representing the reflectivity model
+        of the subsurface.
+    x_ini : float
+        The x coordinate of the first trace in the seismic data.
+    z_ini : float
+        The z coordinate of the first sample in the seismic data.
+    dx : float
+        The x sampling interval in the seismic data.
+    dz : float
+        The z sampling interval in the seismic data.
+    x0 : float
+        The x coordinate of the central point of the slant stack line. 
+    z0 : float
+        The z coordinate of the central point of the slant stack line.
+    pmin, pmax : float
+        Minimum and maximum values of the slopes to consider.
+    dp : float
+        Step size for the slope range.
+
+    Returns:
+    --------
+    Smax : float
+        Maximum coherence value obtained for the given slope range.
+    pmax : float
+        Slope corresponding to the maximum slant stack value.
+    """
     
-    Entrada:
-    data1 -
-    x_ini -
-    z_ini -
-    dx -
-    x0 -
-    y0 -
-    pmin -
-    pmax -
-    dp -
-    
-    Saída:
-    Smax - 
-    pmax - inclinação que gera a semblance máxima
-    """ 
     ps = np.arange(pmin,pmax,dp)
         
     Smax = 0
@@ -181,21 +286,49 @@ def slant_local_p(data1,x_ini,z_ini,dx,dz,x0,z0,pmin,pmax,dp):
             
     return Smax,pmax
 
+
 def local_windowS(data1,xwin,zwin,x_ini,z_ini,dx,dz,x0,z0,pmin,pmax,dp):
+    
     """
-    Define a nova matriz a partir da janela definida para cálculo do slant_local_p()
+    Computes the slant stack maximum value and corresponding p value for 
+    a local window centered at (x_ini, z_ini). 
     
-    Entrada:
-    data1: dado, conferir se precisa ser data1.T
-    xwin: tamanho da janela em x (grid coord.)
-    zwin: tamanho da janela em t (grid coord.)
-    dx,dz: discretização do modelo data1
-    dp: referente ao angulo (cos(theta)*dt/dx)
-    tau:
-    z_ini, x_ini: (grid coord.)
+    The window is defined as data1[z_ini:(z_ini+zwin),x_ini:(x_ini+xwin)].
     
-    Saída: 
-    m_taumax e m_pmax, em que cada célula armazena os valores máximos de tau e p para aquele ponto no modelo
+    Auxiliary function used by `local_window`.
+    
+    Parameters:
+    -----------
+    data1 : numpy.ndarray of shape (nz1, ntr1)
+        A 2D array of shape [nz,nx] representing the reflectivity model
+        of the subsurface.
+    xwin : int
+        The width of the window in number of samples along the x-axis.
+    zwin : int
+        The height of the window in number of samples along the z-axis.
+    x_ini : float
+        The x coordinate of the first trace in the seismic data.
+    z_ini : float
+        The z coordinate of the first sample in the seismic data.
+    dx : float
+        The x sampling interval in the seismic data.
+    dz : float
+        The z sampling interval in the seismic data.
+    x0 : float
+        The x coordinate of the central point of the slant stack line. 
+    z0 : float
+        The z coordinate of the central point of the slant stack line.
+    pmin, pmax : float
+        Minimum and maximum values of the slopes to consider.
+    dp : float
+        Step size for the slope range.
+            
+    Returns:
+    --------
+    s : float
+        The maximum cohernece value.
+    pm : float
+        Slope corresponding to the maximum slant stack value.
     """
     
     W=data1[z_ini:(z_ini+zwin),x_ini:(x_ini+xwin)] 
@@ -206,15 +339,48 @@ def local_windowS(data1,xwin,zwin,x_ini,z_ini,dx,dz,x0,z0,pmin,pmax,dp):
 
 def local_window(data1,xwin,zwin,x_ini,z_ini,dx,dz,pmin,pmax,dp):
     """
-    data1: dado, conferir se precisa ser data1.T
-    xwin: tamanho da janela em x (grid coord.)
-    zwin: tamanho da janela em t (grid coord.) 
-    pmin,pmax,dp: referente ao angulo (cos(theta)*dt/dx)
-    dx,dz: discretização do modelo data1
+    Compute local slant stack for a given model.
     
-    Retorna duas matrizes com dimensões de data1: 
-    m_taumax e m_pmax, em que cada célula armazena os valores máximos de tau e p para aquele ponto no modelo
+    Parameters:
+    -----------
+    data1 : numpy.ndarray of shape (nz1, ntr1)
+        A 2D array of shape [nz,nx] representing the reflectivity model
+        of the subsurface.
+    xwin : int
+        The width of the window in number of samples along the x-axis.
+    zwin : int
+        The height of the window in number of samples along the z-axis.
+    x_ini : float
+        The x coordinate of the first trace in the seismic data.
+    z_ini : float
+        The z coordinate of the first sample in the seismic data.
+    dx : float
+        The x sampling interval in the seismic data.
+    dz : float
+        The z sampling interval in the seismic data.
+    x0 : float
+        The x coordinate of the central point of the slant stack line. 
+    z0 : float
+        The z coordinate of the central point of the slant stack line.
+    pmin, pmax : float
+        Minimum and maximum values of the slopes to consider.
+    dp : float
+        Step size for the slope range.
+        
+    Returns:
+    --------
+    p_max : ndarray of shape (ntr1, nz1); matrix
+        Maximum slope values computed from local windowed slant stacks.
+    s_max : ndarray of shape (ntr1, nz1); matrix
+        Maximum correlation coefficients computed from local windowed slant stacks.
+        
+    Notes:
+    ------
+    - The `pmax` matrix is used for the calculation of the horizontal and vertical components
+    of reflector normal, necessary for the anti-stationary phase filter w(s,x,r) construction. 
+    
     """
+    
     p = np.arange(pmin,pmax,dp)
     
     [ntr1,nz1] = data1.T.shape
